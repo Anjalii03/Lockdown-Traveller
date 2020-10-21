67b468fb68c14cfa0c7e9ee5ddcd30b3451ef2fd
Priyanshi <priyanshi2k@gmail.com>
2020-10-21 13:42:26 +0530

    removerd extra lines

--- Client/Client.java
+++ Client/Client.java
@@ -9,9 +9,9 @@ public class Client {
     public static void main(String args[]) {
         try {
             socket = new Socket("localhost", 6090);
-          //   BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        
             System.out.println("Client created.");
-          //  new Client().logreq(socket,bufferedReader);
+        
            new Client().login();
         } catch (IOException e) {
             System.out.println(e);
@@ -21,25 +21,6 @@ public class Client {
     public void login() throws IOException{
         System.out.println("in login area");
         Login log=new Login();
-        log.setVisible(true);     
-            /*ObjectOutputStream objectOutputStream =new ObjectOutputStream(socket.getOutputStream());
-            LoginRequest lg=new LoginRequest(LoginRequest.username,LoginRequest.password);
-            objectOutputStream.writeObject(lg);
-            objectOutputStream.flush();   */
-        
+        log.setVisible(true);                 
     }
-    
-    
-    /*public void logreq(Socket socket,BufferedReader bufferedReader) throws IOException {
-        ObjectOutputStream objectOutputStream =
-                new ObjectOutputStream(socket.getOutputStream());
-        while(true){
-               user = bufferedReader.readLine();
-               pass = bufferedReader.readLine();
-               LoginRequest lg=new LoginRequest(user,pass);
-               objectOutputStream.writeObject(lg);
-               objectOutputStream.flush();
-        }
-         
-    }*/
 }
